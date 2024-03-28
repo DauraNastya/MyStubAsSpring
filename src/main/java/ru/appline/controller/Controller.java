fe787fe3d1382949eaab6716b75c733610429526
Anastasiia <Nastya-Lazareva@mail.ru>
2024-03-29 00:16:00 +0700

    Method 'createPet' was modified.

--- src/main/java/ru/appline/controller/Controller.java
+++ src/main/java/ru/appline/controller/Controller.java
@@ -1,5 +1,6 @@
 package ru.appline.controller;
 
+import javafx.util.Pair;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,13 +16,19 @@ public class Controller {
     private static final PetModel petModal = PetModel.getInstance();
     private static final AtomicInteger newId = new AtomicInteger(1);
 
-    @PostMapping(value = "/createPet", consumes = "application/json")
-    public void createPet(@RequestBody Pet pet) {
-        petModal.add(pet, newId.getAndIncrement());
+    @PostMapping(value = "/createPet", consumes = "application/json", produces = "application/json")
+    public Pair<String, String> createPet(@RequestBody Pet pet) {
+        int id = newId.getAndIncrement();
+        petModal.add(pet, id);
+        if (id == 1) {
+            return new Pair<String, String>("description", "Первый питомец успешно создан!");
+        } else {
+            return new Pair<String, String>("description", "Питомец успешно создан!");
+        }
     }
 
     @GetMapping(value = "/getAll", produces = "application/json")
-    public Map<Integer, Pet> getAll(){
+    public Map<Integer, Pet> getAll() {
         return petModal.getAll();
     }
 
@@ -31,7 +38,7 @@ public class Controller {
         }
      */
     @GetMapping(value = "/getPet", consumes = "application/json", produces = "application/json")
-    public Pet getPet(@RequestBody Map<String, Integer> id){
+    public Pet getPet(@RequestBody Map<String, Integer> id) {
         return petModal.getFromList(id.get("id"));
     }
 }
